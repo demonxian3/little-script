de2b81ace19d9508bf91eead0e9532885515b672
demonxian3 <920248921@qq.com>
2019-05-07 10:54:06 +0800

    Update db.sh
    ERROR CODE , Function encapsulation

--- shell/db.sh
+++ shell/db.sh
@@ -1,201 +1,212 @@
 #!/bin/bash
-#name:    qucik sql execution in shell
-#version: 1.4
+#version: 2.0
 #author:  Khazix Li
 #date:    2019/05/05
-#help:    please change $username, $password, $database on line:8,9,10
 
 #config
-username=<your username>
-password=<your password>
-database=<your default database>
+version="2.0"                   #shell version
+username=root                   #username
+password=root                   #password
+database=mysql                  #default database
+table=""                        #default table
+where="1=1"                     #default where condition
+column="*"                      #default column
+tableChar="utf-8"               #default Character set for creating table
+tableEngine="InnoDb"            #default engine for creating table
+
+
+#error code
+NOR_MSG_SHOWSQL=101
+NOR_MSG_SHOWRES=102
+NOR_MSG_SHOWVAR=103
+WAR_OPR_CONFIRM=301
+WAR_OPT_INVALID=302
+ERR_ACT_INVALID=501
+ERR_OPT_REQUIRE=502
+ERR_VAL_REQUIRE=503
 
 function help(){
-cat <<EOF
-usage: db <action> [<key> <value>]*
-action:
-    [record]
-        sr => select \$c from \$d.\$t [where \$if]
-        ir => insert into \$d.\$t(\$c) values(\$v)
-        ur => update \$d.\$t set (\$v) [where \$if]
-        dr => delete from \$d.\$t [where \$if]
 
-    [tables]
-        st  => show tables
-        dt  => drop table \$d.\$t
-        ct  => create table \$d.\$t(\$c)
-        sct => show create table \$d.\$t
-        dst => desc \$d.\$t
-
-    [database]
-        sd => show databases
-        cd => create database \$d
-        dd => drop database \$d
-
-    [columns]
-        cc => select ... from infor~.columns where table_name = \$d.\$t
-
-    [variable]
-        sv => show variables like '%\$v%'
-        version => select version();
-
-    [function]
-        sql    => execute \$v;
-        login  => enter interact shell
-        backup => mysqldump \$d
-        source => source \$v
-        passwd => mysqladmin \$v
-EOF
-exit;
-}
-
-if [[ ! -n $1 ]];then
-    help
+if [[ -n $a ]];then
+    case $a in 
+        'sr')      echo "select \$c from \$d.\$t [where \$w]";exit ;;
+        'ir')      echo "insert into \$d.\$t(\$c) values(\$v)";exit ;;
+        'ur')      echo "update \$d.\$t set (\$v) [where \$w]";exit ;;
+        'dr')      echo "delete from \$d.\$t [where \$w]";exit ;;
+        'st')      echo "show tables";exit ;;
+        'dt')      echo "drop table \$d.\$t";exit ;;
+        'ct')      echo "create table \$d.\$t(\$c)";exit ;;
+        'sct')     echo "show create table \$d.\$t";exit ;;
+        'dst')     echo "desc \$d.\$t";exit ;;
+        'sd')      echo "show databases";exit ;;
+        'cd')      echo "create database \$d";exit ;;
+        'dd')      echo "drop database \$d";exit ;;
+        'cc')      echo "select from infor where tname = \$d.\$t";exit ;;
+        'sv')      echo "show variables like '%\$v%'";exit ;;
+        'sql')     echo "execute \$v;";exit ;;
+        'into')    echo "into interact shell";exit ;;
+        'bak')     echo "mysqldump \$d";exit ;;
+        'src')     echo "source \$v";exit ;;
+        'pwd')     echo "mysqladmin \$v";exit ;;
+        'ver')     echo "select version();";exit ;;
+    esac
 fi
 
-action=$1
-shift
+    cat <<EOF
+Usage: db [OPTION]... 
+  -c,  --column              specify column
+  -t,  --table               specify table 
+  -d,  --database            specify database
+  -v,  --value               specify value
+  -f,  --field               select the field on cut command
+  -g,  --format              format output the execution result,
+                             actually equivalent to adding '\\G' in 
+                             the end of SQL 
+  -y,  --yes                 data write operation needs to be confirm
+                             use -y say yes, do it.
+  -w,  --where               specify condition for SQL operation
+  -h,  --help                use -a option get help for action
+  -a,  --action              record action: sr,ir,ur,dr
+                             table action: st,dt,ct,dst,sct
+                             database action: sd,cd,dd
+                             column action: cc
+                             variables: sv
+                             mysql function: sql,into,src,pwd,bak
+                             show shell variables: .v .u .d
+EOF
+    exit;
+}
 
-while [[ -n "$1" && -n "$2" ]];do
+function showMessage(){
     case $1 in
-        "d")   d=$2   ;;
-        "t")   t=$2   ;;
-        "c")   c=$2   ;;
-        "v")   v=$2   ;;
-        "f")   f=$2   ;;
-        "if")  if=$2  ;;
-        *) echo [WARNING]  $1 is invalid key ;;
+    101)echo "$2";;    #[SQL:101] 
+    102)echo "$2";;    #[RES:102] 
+    103)echo "$2";;    #[VAR:103] 
+    301)echo "[WAR:301] confirm operation, use -y make sure to do it"; noConfirm=1; ;;
+    302)echo "[WAR:302] $2 is invalid option.";                                     ;;
+    501)echo "[ERR:501] Invalid action\n";showActionList;exit          ;;
+    502)echo "[ERR:502] $2 option is required"; exit;                               ;;
+    503)
+        echo "[ERR:503] option $2 need specify a value, use -h for help";
+        if [[ $2 -eq '-a' ]];then showActionList;fi;
+        exit;
+    ;;
     esac
-    shift 2;
+}
+
+function showActionList(){
+    echo '[st|dt|ct|dst|sct]'
+    echo '[sr|dr|ur|ir] '
+    echo '[sd|dd|cd|cc|sv]'
+    echo '[sql|bak|src|pwd|into]'
+    echo '[.v|.u|.d]'
+}
+
+function checkOption(){
+    while [[ -n "$1" ]];do
+        case $1 in 
+            '-a') if [[ -z $a ]];then showMessage $ERR_OPT_REQUIRE '-a'; fi; ;;
+            '-d') if [[ -z $d ]];then showMessage $ERR_OPT_REQUIRE '-d'; fi; ;;
+            '-t') if [[ -z $t ]];then showMessage $ERR_OPT_REQUIRE '-t'; fi; ;;
+            '-c') if [[ -z $c ]];then showMessage $ERR_OPT_REQUIRE '-c'; fi; ;;
+            '-w') if [[ -z $w ]];then showMessage $ERR_OPT_REQUIRE '-w'; fi; ;;
+            '-v') if [[ -z $v ]];then showMessage $ERR_OPT_REQUIRE '-v'; fi; ;;
+            '-y') if [[ -z $y ]];then showMessage $WAR_OPR_CONFIRM '-y'; fi; ;;
+        esac
+        shift;
+    done
+}
+
+function setIfDefault(){
+    while [[ -n "$1" ]];do
+        case $1 in 
+            '-d') if [[ -z $d ]];then d="$database";else d="$d" ;fi; ;;
+            '-t') if [[ -z $t ]];then t="$table"   ;else t="$t" ;fi; ;;
+            '-c') if [[ -z $c ]];then c="$column"  ;else c="$c" ;fi; ;;
+            '-w') if [[ -z $w ]];then w="$where"   ;else w="$w" ;fi; ;;
+        esac
+        shift;
+    done
+}
+
+if [[ -z $1 ]];then help; fi 
+
+while [[ -n "$1" ]];do
+    case $1 in
+        "-a")  if [[ -n "$2" ]]; then a=$2;shift 2; else showMessage $ERR_VAL_REQUIRE '-a'; fi ;;
+        "-d")  if [[ -n "$2" ]]; then d=$2;shift 2; else showMessage $ERR_VAL_REQUIRE '-d'; fi ;;
+        "-t")  if [[ -n "$2" ]]; then t=$2;shift 2; else showMessage $ERR_VAL_REQUIRE '-t'; fi ;;
+        "-c")  if [[ -n "$2" ]]; then c=$2;shift 2; else showMessage $ERR_VAL_REQUIRE '-c'; fi ;;
+        "-v")  if [[ -n "$2" ]]; then v=$2;shift 2; else showMessage $ERR_VAL_REQUIRE '-v'; fi ;;
+        "-f")  if [[ -n "$2" ]]; then f=$2;shift 2; else showMessage $ERR_VAL_REQUIRE '-f'; fi ;;
+        "-w")  if [[ -n "$2" ]]; then w=$2;shift 2; else showMessage $ERR_VAL_REQUIRE '-w'; fi ;;
+        "-g")  g=1 ;         shift 1;;
+        "-y")  y=1 ;         shift 1;;
+        "-h")  help;         shift 1;;
+        *) showMessage $WAR_OPT_INVALID $1; shift 1;;
+    esac
 done
 
-sql=""
-needConfirm="0"
-function checkDatabase(){
-    if [[ -z $d ]];then
-        echo '[key:d] database cannot empty';
-        exit;
-    fi
-}
+checkOption '-a';
 
-function checkTable(){
-    if [[ -z $t ]];then
-        echo '[key:t] table cannot empty';
-        exit;
-    fi
-}
-
-function checkColumn(){
-    if [[ -z $c ]];then
-        echo '[key:c] column cannot empty';
-        exit;
-    fi
-}
-
-function checkWhere(){
-    if [[ -z $if ]];then
-        echo '[key:if] condition cannot empty, if you want to delete data, please add "if all"';
-        exit;
-    fi;
-}
-function checkValue(){
-    if [[ -z $v ]];then
-        echo '[key:v] value cannot empty';
-        exit;
-    fi
-}
-
-function defaultField(){
-    if [[ -z $c ]];then
-        c="*";
-    fi
-}
-
-function defaultDatabase(){
-    if [[ -z $d ]];then
-        d="$database";
-    fi
-}
-
-case $action in
+case $a in
     "sr")
-        checkTable; defaultField;
-
-        if test -z $if ; then
-            sql="select $c from $d.$t"
-        else
-            sql="select $c from $d.$t where $if"
-        fi
+        checkOption '-t';
+        setIfDefault '-c' '-w' '-d';
+        sql="select $c from $d.$t where $w"
         ;;
     "ir")
-        needConfirm=1
-        checkTable; checkValue;
-
-        if test -z $c ; then
-            sql="insert into $d.$t values($v);"
-        else
-            sql="insert into $d.$t($c) values($v);"
-        fi
+        checkOption '-t' '-v' '-y';
+        setIfDefault '-d';
+        sql="insert into $d.$t($c) values($v);"
         ;;
     "ur")
-        needConfirm=1
-        checkTable; checkValue;
-
-        if test -z $if ; then
-            sql="update $d.$t set $v;"
-        else
-            sql="update $d.$t set $v where $if";
-        fi
+        checkOption '-t' '-v' '-y';
+        setIfDefault '-d' '-w';
+        sql="update $d.$t set $v where $w";
         ;;
     "dr")
-        needConfirm=1
-        checkTable; checkWhere;
-
-        if [ $if == "all" ];then
-            sql="delete from $d.$t;";
-        else
-            sql="delete from $d.$t where $if;";
-        fi
+        checkOption '-t' '-w' '-y';
+        setIfDefault '-d'
+        sql="delete from $d.$t where $w;";
     ;;
     "dt")
-        checkTable;
-        needConfirm=1
+        checkOption '-t' '-y';
+        setIfDefault '-d';
         sql="drop table $d.$t;";
     ;;
     "ct")
-        checkTable;
-        checkColumn;
-        needConfirm=1
+        checkOption '-t' '-c' '-y';
+        setIfDefault '-d';
         sql="create table $d.$t ($c)";
     ;;
     "st")
-        sql="show tables;";
+        setIfDefault '-d';
+        sql="use $d;show tables;";
     ;;
     "dst")
-        checkTable;
-        sql="desc $t";
+        checkOption '-t';
+        setIfDefault '-d';
+        sql="desc $d.$t";
     ;;
     "sct")
-        checkTable;
+        checkOption '-t';
+        setIfDefault '-d';
         sql="show create table $d.$t";
     ;;
     "sd")
         sql="show databases;";
-        execDirect=1;
     ;;
     "cd")
-        checkDatabase;
-        execDirect=1;
+        checkOption '-d';
         sql="create database $d;";
     ;;
     "dd")
-        checkDatabase;
-        needConfirm=1;
-        execDirect=1
+        checkOption '-d' '-y';
         sql="drop database $d;";
     ;;
     "cc")
-        checkTable;
+        checkOption '-t';
         sql="select distinct 
             ORDINAL_POSITION, COLUMN_NAME,
             IS_NULLABLE, COLUMN_TYPE,
@@ -205,41 +216,41 @@ case $action in
             where table_name = '$t'";
     ;;
     "sv")
-        if [[ -z $v ]];then
-            sql="show variables;"
-        else
-            sql="show variables like '%$v%'";
-        fi
+         sql="show variables like '%$v%'";
     ;;
-    "login")
-        mysql -u$username -p$password -A
+    "into")
+        mysql -u$username -p$password -A;exit;
+    ;;
+    "bak")
+        checkOption '-d';
+        mysqldump -u$username -p$password $d > $d-$(date +%Y%m%d).sql
         exit;
     ;;
-    "backup")
-        checkDatabase;
-        mysqldump -u$username -p$password $d > $d.sql
+    "pwd")
+        checkOption '-v';
+        mysqladmin -u$username -p$password password $v;
+        if [[ $? -eq 0 ]];then sed -i "s/$password/$v/g" $0; fi;
         exit;
     ;;
-    "passwd")
-        checkValue;
-        sed -i "s/$password/$v/g" $0
-        mysqladmin -u$username -p$password password $v
-        exit;
-    ;;
-    "source")
-        checkDatabase;
-        checkValue;
-        needConfirm=1;
-        execDirect=1;
+    "src")
+        checkOption '-d' '-v' '-y';
         sql="use $d;source $v;"
     ;;
     "sql")
-        needConfirm=1
-        checkValue;
+        checkOption '-d' '-v' '-y';
         sql="$v";
     ;;
+    ".v")
+        showMessage $NOR_MSG_SHOWVAR $version
+    ;;
+    ".u")
+        showMessage $NOR_MSG_SHOWVAR "$username  $password"
+    ;;
+    ".d")
+        showMessage $NOR_MSG_SHOWVAR $database
+    ;;
     *)
-        help;
+        showMessage $ERR_ACT_INVALID $a;
     ;;
 esac
 
@@ -247,26 +258,14 @@ esac
 #display
 echo  "$sql"
 
+
 #confirm
-if [[ $needConfirm -eq 1 ]];then
-    if [[ $1 != "ok" ]];then
-        echo "[confirm]? please input 'ok' at the end of the command";
-        exit;
-    fi
-fi
+if [[ $noConfirm -eq 1 ]];then exit; fi;
 
 #format
-if [[ $1 == "g" ]];then
-    sql="$sql\\G";
-fi
+if [[ $g == "1" ]];then sql="$sql\\G"; fi
 
-#execute
-if [[ $execDirect -eq 1 ]];then
-    result=$(echo "$sql" | mysql -u"$username" -p"$password" -A)
-else
-    defaultDatabase;
-    result=$(echo "use $d;$sql" | mysql -u"$username" -p"$password" -A)
-fi
+result=$(echo "$sql" | mysql -u"$username" -p"$password" -A);
 
 #output
 result="${result//\`/}"
