3f3c6754b16b6242806bca31112d276f263a36de
demonxian3 <920248921@qq.com>
2019-05-17 15:47:58 +0800

    2.5 -> 4.0

--- shell/db.sh
+++ shell/db.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-#version: 2.5
+#version: 4.0
 #author:  Khazix Li
-#date:    2019/05/07
+#date:    2019/05/17
 
 #config
-version="2.5"                   #shell version
+version="4.0"                   #shell version
 username=root                   
-password=root       
-database=mysql       
+password=30caa3216fbb4bac       
+database=newzhnc_xyqkl_cn       
 host="localhost"                #default host
-table=""                        #default table
+table="user"                    #default table
 where="1=1"                     #default where condition
 column="*"                      #default column
 tbChar="utf8"                   #default character set for creating table: [latin1], [utf8] [gbk]
 tbEngine="InnoDB"               #default engine for creating table: [InnoDB], [MYISAM], [Memory]
 dbChar="utf8"                   #default character set for creating database: [latin1], [utf8] [gbk]
 dbCollate="utf8_general_ci"     #default collate: [utf8_general_ci] [gbk_chinese_ci] 
+
+dbCacheFile="/tmp/db.tmp"
+tbCacheFile="/tmp/tb.tmp"
 
 #global variables
 d="";
@@ -26,7 +29,8 @@ u="";
 v="";
 sql="";
 res="";
-errno=0;
+db_arr="";
+tb_arr="";
 
 #color variables
 ccls="\033[00m"
@@ -37,23 +41,25 @@ fblu="\033[34m"
 fpin="\033[35m"
 fyan="\033[36m"
 fgra="\033[37m"
-bbla="\033[40m"
-bred="\033[41m"
-bvio="\033[42m"
-byel="\033[43m"
-bblu="\033[44m"
-bpin="\033[45m"
-byan="\033[46m"
-bgra="\033[47m"
+bbla="\033[40;37;1m"
+bred="\033[41;37;1m"
+bvio="\033[42;37;1m"
+byel="\033[43;37;1m"
+bblu="\033[44;37;1m"
+bpin="\033[45;37;1m"
+byan="\033[46;37;1m"
+bgra="\033[47;37;1m"
 
 #error variables
 NOR_PUTMSG=100
 NOR_PUTVAR=101
 NOR_PUTSQL=102
 NOR_PUTRES=103
-NOR_PUTEND=109
+NOR_HITDBN=104
+NOR_HITTBN=105
 WAR_OPRCFM=301
 WAR_OPTIVL=302
+WAR_CACNUL=303
 ERR_ACTIVL=501
 ERR_OPTREQ=502
 ERR_VALREQ=503
@@ -84,11 +90,12 @@ if [[ -n $a ]];then
         'sg')      echo "show grants for \$u";;
         'up')      echo "set password for \$u = password('\$v')"; ;;
         'sql')     echo "mysql> \$v;"; ;;
-        'into')    echo "into interact shell"; ;;
+        'sel')     echo "into interact shell"; ;;
         'bak')     echo "mysqldump \$d"; ;;
         'chp')     echo "mysqladmin -u\$usr -p\$pwd \$v \$d"; ;;
         'src')     echo "source \$v"; ;;
         'ver')     echo "select version();"; ;;
+        *)         putActLst; ;;
     esac
     exit;
 fi
@@ -114,7 +121,7 @@ Usage: db [OPTION]...
                              database action: sd,cd,dd
                              column action: cc
                              variables: sv
-                             mysql function: sql,into,src,bak
+                             mysql function: sql,shl,src,bak
                              show shell variables: .v .u .d
 EOF
     exit;
@@ -127,7 +134,7 @@ function putActLst(){
     echo '[table]    st dt ct cc dst sct '
     echo '[record]   sr dr ur ir  '
     echo '[database] sd dd cd sv '
-    echo '[function] sql bak src into chp '
+    echo '[function] sql bak src shl chp ver '
     echo '[user]     cu du up gu sg su up '
     echo '[shell]    .v .u .d '
     echo -e $ccls;
@@ -135,19 +142,20 @@ function putActLst(){
 
 
 function putMsg(){
-    case $errno in
+    case $1 in
     $NOR_PUTMSG)  echo $2; ;;
-    $NOR_PUTEND)  echo -e $ccls; ;;
-    $NOR_PUTVAR)  echo -e "$bbla[VAR:$errno]$ccls$2"; ;;
-    $NOR_PUTSQL)  echo -e "$bblu[SQL:$errno]$ccls$fblu$sql$ccls"; ;;
-    #$NOR_PUTRES)  echo -e "$bvio[RES:$errno]$ccls\n$fvio$res$ccls"; ;;
-    $NOR_PUTRES)  echo -e "$bvio[RES:$errno]$ccls\n$fvio"; ;;
-    $WAR_OPRCFM)  echo -e "$byel[WAR:$errno]$ccls$fyel use -y make sure to do it$ccls"; noConfirm=1; ;;
-    $WAR_OPTIVL)  echo -e "$byel[WAR:$errno]$ccls$fyel -$1 is invalid option.";                 $ccls ;;
-    $ERR_ACTIVL)  echo -e "$bred[ERR:$errno]$ccls$fred Invalid action$ccls";putActLst;exit      $ccls ;;
-    $ERR_OPTREQ)  echo -e "$bred[ERR:$errno]$ccls$fred -$1 option is required$ccls"; exit;      $ccls ;;
-    $ERR_VALREQ)  echo -e "$bred[ERR:$errno]$ccls$fred option -$1 need specify a value$ccls"    $ccls;
-                  if [[ $1 -eq '-a' ]];then putActLst;fi;
+    $NOR_PUTVAR)  echo -e "$bbla[NOR_PUTVAR]$ccls$fbla$2$ccls";   exit;;
+    $NOR_PUTSQL)  echo -e "$bblu[NOR_PUTSQL]$ccls\n$fblu$2$ccls"; ;;
+    $NOR_PUTRES)  echo -e "$bvio[NOR_PUTRES]$ccls\n$fvio$2";      ;;
+    $NOR_HITTBN)  echo -e "$bpin[CUR_TABLEN]$ccls$fpin$2$ccls";   ;;
+    $NOR_HITDBN)  echo -e "$byan[CUR_DBNAME]$ccls$fyan$2$ccls";   ;;
+    $WAR_OPRCFM)  echo -e "$byel[WAR_OPRCFM]$ccls$fyel use -y make sure to do it$ccls"; noConfirm=1; ;;
+    $WAR_OPTIVL)  echo -e "$byel[WAR_OPTIVL]$ccls$fyel -$2 is invalid option.$ccls";            ;;
+    $WAR_CACNUL)  echo -e "$byel[WAR_OPTIVL]$ccls$fyel $2 isn't hit in cache.$ccls";            ;;
+    $ERR_ACTIVL)  echo -e "$bred[ERR_ACTIVL]$ccls$fred Invalid action$ccls";putActLst;exit      ;;
+    $ERR_OPTREQ)  echo -e "$bred[ERR_OPTREQ]$ccls$fred -$2 option is required$ccls"; exit;      ;;
+    $ERR_VALREQ)  echo -e "$bred[ERR_VALREQ]$ccls$fred option -$2 need specify a value$ccls"    ;
+                  if [[ $2 -eq '-a' ]];then putActLst;fi;
                   exit;
     ;;
     esac
@@ -157,14 +165,14 @@ function chkOpt(){
     #OPT
     while [[ -n "$1" ]];do
         case $1 in 
-            'a') if [[ -z $a ]];then errno=$ERR_OPTREQ;putMsg a; fi; ;;
-            'd') if [[ -z $d ]];then errno=$ERR_OPTREQ;putMsg d; fi; ;;
-            't') if [[ -z $t ]];then errno=$ERR_OPTREQ;putMsg t; fi; ;;
-            'c') if [[ -z $c ]];then errno=$ERR_OPTREQ;putMsg c; fi; ;;
-            'w') if [[ -z $w ]];then errno=$ERR_OPTREQ;putMsg w; fi; ;;
-            'v') if [[ -z $v ]];then errno=$ERR_OPTREQ;putMsg v; fi; ;;
-            'u') if [[ -z $u ]];then errno=$ERR_OPTREQ;putMsg u; fi; ;;
-            'y') if [[ -z $y ]];then errno=$WAR_OPRCFM;putMsg y; fi; ;;
+            'a') if [[ -z $a ]];then putMsg $ERR_OPTREQ a; fi; ;;
+            'd') if [[ -z $d ]];then putMsg $ERR_OPTREQ d; fi; ;;
+            't') if [[ -z $t ]];then putMsg $ERR_OPTREQ t; fi; ;;
+            'c') if [[ -z $c ]];then putMsg $ERR_OPTREQ c; fi; ;;
+            'w') if [[ -z $w ]];then putMsg $ERR_OPTREQ w; fi; ;;
+            'v') if [[ -z $v ]];then putMsg $ERR_OPTREQ v; fi; ;;
+            'u') if [[ -z $u ]];then putMsg $ERR_OPTREQ u; fi; ;;
+            'y') if [[ -z $y ]];then putMsg $WAR_OPRCFM y; fi; ;;
         esac
         shift;
     done
@@ -229,151 +237,110 @@ function setSql(){
      ;;
     'src')sql="USE $d;SOURCE $v";
      ;;
+    'ver')sql="SELECT @@version";
+     ;;
     'sql')sql="$sql";
      ;;
 
     esac;
 }
 
+function setDbn(){
+    if [[ -e $dbCacheFile && $1 -gt 0 ]] 2>/dev/null; then
+        d=`sed -n "${1}p" $dbCacheFile`;
+        if [[ -z $d ]];then
+            putMsg $WAR_CACNUL database;
+            d=$database;
+        fi
+    else
+        d=$1;
+    fi
+}
 
-
+function setTbn(){
+    if [[ -e $tbCacheFile && $1 -gt 0 ]] 2>/dev/null; then
+        t=`sed -n "${1}p" $tbCacheFile`;
+        if [[ -z $t ]];then
+            putMsg $WAR_CACNUL table;
+            t=$table;
+        fi
+    else
+        t=$1;
+    fi
+}
 
 # /*   MAIN   */
 if [[ -z $1 ]];then help; fi 
 while [[ -n "$1" ]];do
     #OPT
     case $1 in
-        "-u")  if [[ -n "$2" ]]; then u=$2;shift 2; else errno=$ERR_VALREQ;putMsg u; fi ;;
-        "-a")  if [[ -n "$2" ]]; then a=$2;shift 2; else errno=$ERR_VALREQ;putMsg a; fi ;;
-        "-d")  if [[ -n "$2" ]]; then d=$2;shift 2; else errno=$ERR_VALREQ;putMsg d; fi ;;
-        "-t")  if [[ -n "$2" ]]; then t=$2;shift 2; else errno=$ERR_VALREQ;putMsg t; fi ;;
-        "-c")  if [[ -n "$2" ]]; then c=$2;shift 2; else errno=$ERR_VALREQ;putMsg c; fi ;;
-        "-v")  if [[ -n "$2" ]]; then v=$2;shift 2; else errno=$ERR_VALREQ;putMsg v; fi ;;
-        "-f")  if [[ -n "$2" ]]; then f=$2;shift 2; else errno=$ERR_VALREQ;putMsg f; fi ;;
-        "-w")  if [[ -n "$2" ]]; then w=$2;shift 2; else errno=$ERR_VALREQ;putMsg w; fi ;;
+        "-d")  if [[ -n "$2" ]]; then setDbn $2;shift 2; else a="sd"; shift 1; fi ;;
+        "-t")  if [[ -n "$2" ]]; then setTbn $2;shift 2; else a="st"; shift 1; fi ;;
+        "-u")  if [[ -n "$2" ]]; then u=$2;shift 2; else putMsg $ERR_VALREQ u; fi ;;
+        "-a")  if [[ -n "$2" ]]; then a=$2;shift 2; else putMsg $ERR_VALREQ a; fi ;;
+        "-c")  if [[ -n "$2" ]]; then c=$2;shift 2; else putMsg $ERR_VALREQ c; fi ;;
+        "-v")  if [[ -n "$2" ]]; then v=$2;shift 2; else putMsg $ERR_VALREQ v; fi ;;
+        "-f")  if [[ -n "$2" ]]; then f=$2;shift 2; else putMsg $ERR_VALREQ f; fi ;;
+        "-w")  if [[ -n "$2" ]]; then w=$2;shift 2; else putMsg $ERR_VALREQ w; fi ;;
         "-g")  g=1 ;         shift 1;;
         "-y")  y=1 ;         shift 1;;
         "-h")  help;         shift 1;;
-        "-st"|"-dt"|"-ct"|"-dst"|"-sct"|"-sr"|"-dr"|"-ur"|"-ir"|"-sd"|"-dd"|"-cd"|"-cc"|"-sv"|"-sql"|"-bak"|"-src"|"-into"|"-cu"|"-du"|"-up"|"-gu"|"-sg"|"-su"|"-chp")
+        "-st"|"-dt"|"-ct"|"-dst"|"-sct"|"-sr"|"-dr"|"-ur"|"-ir"|"-sd"|"-dd"|"-cd"|"-cc"|"-sv"|"-sql"|"-bak"|"-src"|"-shl"|"-cu"|"-du"|"-up"|"-gu"|"-sg"|"-su"|"-chp"|"-ver")
         a="${1:1}"; shift 1;;
-        *) errno=$WAR_OPTIVL; putMsg $1; shift 1;;
+        *)  putMsg $WAR_OPTIVL $1; shift 1;;
     esac
 done
-
 
 chkOpt '-a';
 
 #ACT
 case $a in
-    "sr")
-        chkOpt t;
-        setDef c w d;
-        ;;
-    "ir")
-        chkOpt t v y;
-        setDef d;
-        ;;
-    "ur")
-        chkOpt t v w y;
-        setDef d;
-        ;;
-    "dr")
-        chkOpt t w y;
-        setDef d;
-    ;;
-    "dt")
-        chkOpt t y;
-        setDef d;
-    ;;
-    "ct")
-        chkOpt t c y;
-        setDef d;
-    ;;
-    "st")
-        setDef d;
-    ;;
-    "dst")
-        chkOpt t;
-        setDef d;
-    ;;
-    "sct")
-        chkOpt t;
-        setDef d;
-    ;;
-    "sd")
-    ;;
-    "cd")
-        chkOpt d;
-    ;;
-    "dd")
-        chkOpt d y;
-    ;;
-    "cc")
-        chkOpt t;
-    ;;
-    "sv")
-    ;;
-    "su")
-        setDef w c;
-    ;;
-    "cu")
-        chkOpt u v y;
-    ;;
-    "du")
-        chkOpt u y;
-    ;;
-    "gu")
-        chkOpt u v t d y;
-    ;;
-    "sg")
-        chkOpt u;
-    ;;
-    "up")
-        chkOpt u v y;
-    ;;
-    "into")
-        mysql -u$username -p$password -A;
-        exit;
-    ;;
-    "bak")
-        chkOpt d;
-        mysqldump -u$username -p$password $d > $d-$(date +%Y%m%d).sql
-        exit;
-    ;;
-    "chp")
-        chkOpt v;
-        mysqladmin -u$username -p$password password $v;
-        if [[ $? -eq 0 ]];then sed -i "s/$password/$v/g" $0; fi;
-        exit;
-    ;;
-    "src")
-        chkOpt d v y;
-    ;;
-    "sql")
-        chkOpt d v y;
-    ;;
-    ".v")
-        errno=$NOR_PUTVAR;
-        putMsg $version
-    ;;
-    ".u")
-        errno=$NOR_PUTVAR;
-        putMsg "$username  $password"
-    ;;
-    ".d")
-        errno=$NOR_PUTVAR;
-        putMsg $database
-    ;;
-    *)
-        errno=$ERR_ACTIVL;
-        putMsg $a;
-    ;;
+    "sr")  chkOpt t;       setDef c w d;   ;;
+    "ir")  chkOpt t v y;   setDef d;       ;;
+    "ur")  chkOpt t v w y; setDef d;       ;;
+    "dr")  chkOpt t w y;   setDef d;       ;;
+    "dt")  chkOpt t y;     setDef d;       ;;
+    "ct")  chkOpt t c y;   setDef d;       ;;
+    "st")  setDef d;       ;;
+    "dst") chkOpt t;       setDef d;       ;;
+    "sct") chkOpt t;       setDef d;       ;;
+    "sd")  ;;
+    "cd")  chkOpt d;         ;;
+    "dd")  chkOpt d y;       ;;
+    "cc")  chkOpt t;         ;;
+    "sv")  ;;
+    "su")  setDef w c;       ;;
+    "cu")  chkOpt u v y;     ;;
+    "du")  chkOpt u y;       ;;
+    "gu")  chkOpt u v t d y; ;;
+    "sg")  chkOpt u;         ;;
+    "up")  chkOpt u v y;     ;;
+    "shl") mysql -u$username -p$password -A; exit; ;;
+    "bak") chkOpt d; mysqldump -u$username -p$password $d > $d-$(date +%Y%m%d).sql exit; ;;
+    "src") chkOpt d v y; ;;
+    "sql") chkOpt d v y; ;;
+    ".v")  putMsg $NOR_PUTVAR $version; ;;
+    ".u")  putMsg $NOR_PUTVAR "$username  $password"; ;;
+    ".d")  putMsg $NOR_PUTVAR $database; ;;
+    "ver") ;;
+    "chp") chkOpt v;
+           mysqladmin -u$username -p$password password $v;
+           if [[ $? -eq 0 ]];then sed -i "s/$password/$v/g" $0; fi;
+           exit; ;;
+       *)  putMsg $ERR_ACTIVL $a; ;;
 esac
 
-#showsql
-setSql;
-errno=$NOR_PUTSQL;
-putMsg;
+setSql; 
+
+if [[ -n $d ]];then
+    putMsg $NOR_HITDBN "$d";
+fi
+if [[ -n $t ]];then
+    putMsg $NOR_HITTBN "$t";
+fi
+if [[ -n $sql ]];then
+    putMsg $NOR_PUTSQL "$sql";
+fi
 
 
 #confirm
@@ -382,18 +349,28 @@ if [[ $noConfirm -eq 1 ]];then exit; fi;
 #format
 if [[ $g == "1" ]];then sql="$sql\\G"; fi
 
-
+    
 #output
-errno=$NOR_PUTRES;
-putMsg;
-res=$(echo "$sql" | mysql -u"$username" -p"$password" -A);
-res="${res//\`/}"
-if [[ $f ]];then
-    #echo "$res" | cut -f $f | column -t;
-    echo "$res" | cut -f $f
+if [[ $a == "sd" ]];then
+    res=$(echo "$sql" | mysql -u"$username" -p"$password" -A | tee $dbCacheFile | nl);
+elif [[ $a == "st" ]];then
+    res=$(echo "$sql" | mysql -u"$username" -p"$password" -A | tee $tbCacheFile | nl);
 else
-    echo "$res"
-    #echo "$res" | column -t;
+    res=$(echo "$sql" | mysql -u"$username" -p"$password" -A);
 fi
-errno=$NOR_PUTEND;
-putMsg;
+res="${res//\`/}"
+
+
+echo -e $fvio
+if [[ $f ]];then
+    if [[ $f == "c" ]];then
+        putMsg $NOR_PUTRES "$res" | column -t
+    elif [[ ${f:0:1} == "c" ]];then
+        putMsg $NOR_PUTRES "$res" | cut -f ${f:1} | column -t
+    else
+        putMsg $NOR_PUTRES "$res" | cut -f $f
+    fi
+else
+    putMsg $NOR_PUTRES "$res"
+fi
+echo -e $ccls
