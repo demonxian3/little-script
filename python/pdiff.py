ca1daf920e305d44cc03374e8d28058ebb9b8da7
Khazix Li <920248921@qq.com>
2021-01-20 17:52:32 +0800

    Update and rename scanGitFileChange to pdiff.py

--- python/pdiff.py
+++ python/pdiff.py
@@ -1,12 +1,16 @@
 #coding: utf-8
+#pip install colorama 
 
-import re;
+import os;
 import sys;
 import pprint
 import subprocess;
 from datetime import datetime;
 from optparse import OptionParser
 from threading import Thread
+from colorama import Fore, Style, init
+
+
 
 #d directory
 #s string
@@ -16,6 +20,62 @@ from threading import Thread
 #l link
 #a array
 #dt datetime
+
+class Color:
+    RED     = '';
+    GREEN   = '';
+    YELLOW  = '';
+    BLUE    = '';
+    PINK = '';
+    CYAN    = '';
+    WHITE   = '';
+    bRED     = '';
+    bGREEN   = '';
+    bYELLOW  = '';
+    bBLUE    = '';
+    bPINK = '';
+    bCYAN    = '';
+    bWHITE   = '';
+    RST = '';
+    
+    @staticmethod
+    def setMode(mode):
+        
+        if mode == 1:
+            init(autoreset=True)
+            Color.RED      = Fore.RED;
+            Color.GREEN    = Fore.GREEN;
+            Color.YELLOW   = Fore.YELLOW;
+            Color.BLUE     = Fore.BLUE;
+            Color.bPINK    = Fore.MAGENTA;
+            Color.CYAN     = Fore.CYAN;
+            Color.WHITE    = Fore.WHITE;
+            Color.bRED     = Style.BRIGHT + Fore.RED;
+            Color.bGREEN   = Style.BRIGHT + Fore.GREEN;
+            Color.bYELLOW  = Style.BRIGHT + Fore.YELLOW;
+            Color.bBLUE    = Style.BRIGHT + Fore.BLUE;
+            Color.bbPINK   = Style.BRIGHT + Fore.MAGENTA;
+            Color.bCYAN    = Style.BRIGHT + Fore.CYAN;
+            Color.bWHITE   = Style.BRIGHT + Fore.WHITE;
+            Color.RST = Style.RESET_ALL + Fore.RESET
+            
+            
+        elif mode == 2:
+            Color.RED      = '\033[31m';
+            Color.GREEN    = '\033[32m';
+            Color.YELLOW   = '\033[33m';
+            Color.BLUE     = '\033[34m';
+            Color.PINK     = '\033[35m';
+            Color.CYAN     = '\033[36m';
+            Color.WHITE    = '\033[37m';
+            Color.bRED     = '\033[1;31m';
+            Color.bGREEN   = '\033[1;32m';
+            Color.bYELLOW  = '\033[1;33m';
+            Color.bBLUE    = '\033[1;34m';
+            Color.bMAGENTA = '\033[1;35m';
+            Color.bCYAN    = '\033[1;36m';
+            Color.bWHITE   = '\033[1;37m';
+            Color.RST      = '\033[0m';
 
 class Node:
     def __init__(self, sShortHash, sFullHash, sComment, sTime, sAuthor, pPrev=None, pNext=None):
@@ -32,12 +92,10 @@ class Node:
             dtTime = datetime.fromtimestamp(iTime);
             self.time = dtTime.strftime("%Y-%m-%d %H:%M:%S");
 
-
 class Link:
     def __init__(self):
         self.head = Node('','','','','');
         self.lenght = 0;
-
 
 class Options:
     
@@ -47,12 +105,13 @@ class Options:
             'include': ['array', '包含关键字，多个关键字使用空格分隔，元素间逻辑与'],
             'exclude': ['array', '剔除关键字，多个关键字使用空格分隔'],
             'verbose': ['boolean', '显示扫描详情'],
-            'typesort': ['boolean', '按照文件类型格式'],
+            'type': ['array', '按照文件类型划分显示如: -t biz:/lib/biz -t tsx:.tsx'],
             'rtx': ['string', '提交作者'],
             'all': ['boolean', '全扫描'],
+            'color': ['string', '显示颜色：1 通用兼容颜色，2 linux高亮颜色'],
         }
 
-        usage = "%prog [OPTION]... [COMMIT1] [COMMIT2]";
+        usage = "%prog [OPTION]... [较早的commit] [较晚的commit]";
         parser = OptionParser(usage=usage);
 
         for key in self.options:
@@ -69,9 +128,9 @@ class Options:
                 parser.add_option(sShort, sFull, type="string", dest=key, help=sHelp, default='');
 
         (self.opts, self.args) = parser.parse_args();
-        self.check();
+        self.parse();
 
-    def check(self):
+    def parse(self):
         dOptions = self.opts;
         aArgumnet = self.args;
 
@@ -79,17 +138,22 @@ class Options:
             print("rtx 为必填项，请使用-r/--rtx参数指定你的rtx");
             sys.exit();
 
-        if not dOptions.include and not dOptions.all:
+        if not dOptions.include and not dOptions.all and not aArgumnet:
             print("请至少指定一个过滤关键字，否则历史提交版本数据会很多，使用-i/--include");
             sys.exit();
 
-        self.typesort = dOptions.typesort;
         self.rtx = dOptions.rtx;
         self.include = dOptions.include;
         self.exclude = dOptions.exclude;
         self.verbose = dOptions.verbose;
         self.first = dOptions.first;
+        self.color = dOptions.color;
         self.mode = 'scan-all';
+
+        self.typesort = {};
+        for sType in dOptions.type:
+            part = sType.split(':');
+            self.typesort[part[0]] = part[1];
 
         if aArgumnet :
             if len(aArgumnet) != 2:
@@ -99,13 +163,97 @@ class Options:
             self.startHash = aArgumnet[0];
             self.stopHash = aArgumnet[1];
 
-            if len(startHash) == 40 and len(stopHash) == 40:
+            if len(self.startHash) == 40 and len(self.stopHash) == 40:
                 self.mode = 'scan-fullhash-range';
             else:
                 self.mode = 'scan-shorthash-range';
 
-
 class Pdiff:
+    def __init__(self):
+        self.dFileLog = {'M':{}, 'D':{}, 'R':{}, 'A':{}, 'C':{}, 'T':{}, 'U':{}, 'X':{}, 'B':{}};
+        self.dMergeLog = {'M':{}, 'D':{}, 'R':{}, 'A':{}, 'C':{}, 'T':{}, 'U':{}, 'X':{}, 'B':{}};
+        self.dCommitLog = {};
+        self.options = Options();
+        self.mode = self.options.mode;
+        self.typesort = self.options.typesort;
+        self.openDiff = True;
+        self.hasMergeFile = False;
+
+        if self.mode != 'scan-all':
+            self.openDiff = False;
+            self.startHash = self.options.startHash;
+            self.stopHash = self.options.stopHash;
+
+        
+        Color.setMode(int( self.options.color if self.options.color else 0));
+        lHistoryCommit = Link();
+        pCommit = lHistoryCommit.head;
+
+        sLastPushHash = "";
+        aHistoryCommit = self.getCommitHistory();
+
+        for sCommit in aHistoryCommit:
+            aCommit = sCommit.split('|');
+
+            if len(aCommit) == 0:
+                continue;
+
+            nCommit = Node(*aCommit[:5], pCommit, None);
+            pCommit.next = nCommit;
+            pCommit = nCommit;
+
+            sId = nCommit.id;
+            sHash = nCommit.hash;
+            sAuthor = nCommit.author;
+            sComment = nCommit.comment;
+            dtTime = nCommit.time;
+
+            if "Merge" in sComment and nCommit.prev.hash == sLastPushHash:
+                self.diff(nCommit, nCommit.prev, True);
+            
+            
+            bWillDiff = self.openDiff;
+
+            if self.mode == 'scan-fullhash-range':
+                self.openDiff = True if self.startHash == sHash else self.openDiff ;
+                self.openDiff = False if self.stopHash == sHash else self.openDiff ;
+
+            elif self.mode == 'scan-shorthash-range':
+                self.openDiff = True if self.startHash in sId else self.openDiff ;
+                self.openDiff = False if self.stopHash in sId else self.openDiff ;
+
+
+            self.dCommitLog[sId] = nCommit;
+
+            for sSearch in self.options.include:
+                if sSearch not in (sHash + dtTime + sAuthor + sComment):
+                    bWillDiff = False;
+                    break;
+
+            for sSearch in self.options.exclude:
+                if sSearch in  (sHash + dtTime + sAuthor + sComment):
+                    bWillDiff = False;
+                    break;
+            
+            if sAuthor != self.options.rtx:
+                continue;
+
+            if bWillDiff:
+                if self.options.verbose:
+                    print("正在扫描历史版本: %s %s %s %s" %(sHash, dtTime, sAuthor, sComment))
+                
+                if (not nCommit.hash) or (not nCommit.prev.hash):
+                    continue
+
+                self.diff(nCommit, nCommit.prev);
+                sLastPushHash = nCommit.hash;
+
+        
+        self.showFileLog(self.dFileLog);
+        if self.hasMergeFile:
+            print('\n\n\n*** 以下文件检测于Merge中，可能为其他人修改的文件 ***');
+            self.showFileLog(self.dMergeLog);
+    
     def execute(self, cmd):
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         output, err = p.communicate();
@@ -114,12 +262,13 @@ class Pdiff:
         except:
             return output;
 
-
     def getCommitHistory(self):
         sCmd = 'git log --pretty=format:"%h|%H|%s|%ct|%an" --abbrev-commit --reverse';
         sHistoryCommit = self.execute(sCmd);
+        if not len(sHistoryCommit):
+            print("请在Git项目下执行该命令");
+            sys.exit();
         return sHistoryCommit.split('\n');
-
 
     def diff(self, nCur, nPrev, bMerge=False):
         sId = nCur.id;
@@ -161,17 +310,17 @@ class Pdiff:
                     self.dFileLog[sOpera][sFilename]['rtx'] = sRtx;
             else:
                 if sFilename in self.dFileLog[sOpera]:
+                    self.hasMergeFile = True;
                     self.dMergeLog[sOpera][sFilename] = self.dFileLog[sOpera][sFilename];
                     del self.dFileLog[sOpera][sFilename];
 
-
     def showFileLog(self, dFileLog):
         dOperaMap = {
-            'R': ['重名', '\033[1;33m'],
-            'A': ['添加', '\033[1;32m'],
-            'M': ['修改', '\033[1;36m'],
-            'C': ['复制', '\033[1;33m'],
-            'D': ['移除', '\033[1;35m'],
+            'R': ['重名', Color.bYELLOW  ],
+            'A': ['添加', Color.bGREEN   ],
+            'M': ['修改', Color.bCYAN    ],
+            'C': ['复制', Color.bYELLOW  ],
+            'D': ['移除', Color.bPINK    ],
         }
 
         if not self.options.typesort:
@@ -188,15 +337,15 @@ class Pdiff:
                     sRename = dInfo['rename'];
 
                     if sOpera == 'R':
-                        print("\033[37m[%s] \033[31m(%s) \033[1;34m%-12s\033[0m - %s%s %s => %s\033[0m" % (dtTime, sId, sRtx, sColor, sLabel, sRename, sFilename ));
+                        print("[%s] %s(%s) - %s%s %s => %s%s" % (dtTime, Color.RED, sId,  sColor, sLabel, sRename, sFilename, Color.RST ));
                     else:
-                        print("\033[37m[%s] \033[31m(%s) \033[1;34m%-12s\033[0m - %s%s %s\033[0m" % (dtTime, sId, sRtx, sColor, sLabel, sFilename));
+                        print("[%s] %s(%s) - %s%s %s%s" % (dtTime, Color.RED, sId,  sColor, sLabel, sFilename, Color.RST));
 
         
         else:
             dFileLog = self.sortFileLog(dFileLog);
             for sFileType in dFileLog:
-                print("\033[1;37m" + sFileType + ': \033[0m');
+                print(Color.bWHITE + sFileType + Color.RST);
                 for sFilename in dFileLog[sFileType]:
                     for sOpera in dFileLog[sFileType][sFilename]:
                         sLabel = dOperaMap[sOpera][0];
@@ -209,34 +358,16 @@ class Pdiff:
                         sRename = dInfo['rename'];
 
                         if sOpera == 'R':
-                            print("\033[37m[%s] \033[31m(%s) - %s%s %s => %s\033[0m" % (dtTime, sId, sColor, sLabel, sFilename, sRename));
+                            print("[%s] %s(%s) - %s%s %s => %s%s" % (dtTime, Color.RED, sId,  sColor, sLabel, sRename, sFilename, Color.RST ));
                         else:
-                            print("\033[37m[%s] \033[31m(%s) - %s%s %s\033[0m" % (dtTime, sId, sColor, sLabel, sFilename));
+                            print("[%s] %s(%s) - %s%s %s%s" % (dtTime, Color.RED, sId,  sColor, sLabel, sFilename, Color.RST));
         
-
-
     def sortFileLog(self, dFileLog):
-        dResult = {
-            'js': {},
-            'biz': {},
-            'css': {},
-            'tpl': {},
-            'cfg': {},
-            'model': {},
-            'controller':{},
-            'other':{},
-        }
+        dResult = {'other':{}}
+        dCheckKey = self.typesort;
 
-
-        dCheckKey = {
-            'js': '.js',
-            'biz': '/lib/biz',
-            'cfg': '.cfg',
-            'css': '.css',
-            'tpl': '.tpl',
-            'model': 'Model.class.php',
-            'controller': 'Controller.php',
-        }
+        for key in self.typesort:
+            dResult[key] = {};
 
         for sOpera in dFileLog:
             for sFilename in dFileLog[sOpera]:
@@ -258,69 +389,6 @@ class Pdiff:
 
         return dResult;
 
-                    
 
-    def __init__(self):
-        self.dFileLog = {'M':{}, 'D':{}, 'R':{}, 'A':{}, 'C':{}, 'T':{}, 'U':{}, 'X':{}, 'B':{}};
-        self.dMergeLog = {'M':{}, 'D':{}, 'R':{}, 'A':{}, 'C':{}, 'T':{}, 'U':{}, 'X':{}, 'B':{}};
-        self.dCommitLog = {};
-        self.options = Options();
-
-        lHistoryCommit = Link();
-        pCommit = lHistoryCommit.head;
-
-        sLastPushHash = "";
-        aHistoryCommit = self.getCommitHistory();
-
-        for sCommit in aHistoryCommit:
-            aCommit = sCommit.split('|');
-
-            if len(aCommit) == 0:
-                continue;
-
-            nCommit = Node(*aCommit[:5], pCommit, None);
-            pCommit.next = nCommit;
-            pCommit = nCommit;
-
-            sId = nCommit.id;
-            sHash = nCommit.hash;
-            sAuthor = nCommit.author;
-            sComment = nCommit.comment;
-            dtTime = nCommit.time;
-
-            if "Merge" in sComment and nCommit.prev.hash == sLastPushHash:
-                self.diff(nCommit, nCommit.prev, True);
-            
-            bWillPush = True;
-            self.dCommitLog[sId] = nCommit;
-
-            for sSearch in self.options.include:
-                if sSearch not in (sHash + dtTime + sAuthor + sComment):
-                    bWillPush = False;
-                    break;
-
-            for sSearch in self.options.exclude:
-                if sSearch in  (sHash + dtTime + sAuthor + sComment):
-                    bWillPush = False;
-                    break;
-            
-            if sAuthor != self.options.rtx:
-                continue;
-
-            if bWillPush:
-                if self.options.verbose:
-                    print("正在扫描历史版本: %s %s %s %s" %(sHash, dtTime, sAuthor, sComment))
-                
-                if (not nCommit.hash) or (not nCommit.prev.hash):
-                    continue
-
-                self.diff(nCommit, nCommit.prev);
-                sLastPushHash = nCommit.hash;
-
-        
-        self.showFileLog(self.dFileLog);
-        print('\n\n\n*** 以下文件检测于Merge中，可能为其他人修改的文件 ***');
-        self.showFileLog(self.dMergeLog);
-    
 if __name__ == '__main__':
     Pdiff();
